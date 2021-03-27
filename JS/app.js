be05e1eb4fca9d7ed91f4e74d0f8bde8fb2f3353
Garry <gurjyotsingh76@gmail.com>
2021-03-27 12:01:44 +0530

    adding second loop

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,20 @@
 // your JavaScript goes here, you can delete this comment
 
-const todoList = [
+const todolist = [
     {todo:"clone a repository", status:"complete"},
     {todo:"link the css and js to html", status:"complete"},
     {todo:"commit code at appropriate intervals", status:"started"},
     {todo:"push code to git repo", status:"started"}
-]
+;]
 
-const completedTodos = []
+const completedTodos = [];
+
+for (item in todolist) {
+    if (todolist[item].status === "complete") {
+      completedTodos.push(todolist[item])
+    }
+  }
+
+for (item in todolist) {
+    console.log(todolist(item).todo)
+}
