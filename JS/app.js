9f3ccad6985630c6f3c84123f1dbe3c82114685c
Garry <gurjyotsingh76@gmail.com>
2021-03-27 11:32:19 +0530

    creating arrays in js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,10 @@
 // your JavaScript goes here, you can delete this comment
+
+const todoList = [
+    {todo:"clone a repository", status:"complete"},
+    {todo:"link the css and js to html", status:"complete"},
+    {todo:"commit code at appropriate intervals", status:"started"},
+    {todo:"push code to git repo", status:"started"}
+]
+
+const completedTodos = []
